39275870c43ce36b5c709ce04fc13062383d1a66
cleberfarias <cleber_afd@hotmail.com>
2025-05-20 10:05:47 -0300

    refatorando o código e tratamento de erro com try catch

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const containerVideos = document.querySelector(".videos__container")
+const containerVideos = document.querySelector(".videos__container");
 
-async function buscarVidosAPI (){
-const buscaVideo = await fetch("http://localhost:3000/videos")
-const videos = await buscaVideo.json()
-    
-        videos.forEach((video)=>{ //para cada video vai ser exibido // o innerHTML vai colocar os videos dentro do HTML - cada video vai ser acrescendo 
-            containerVideos.innerHTML += `
-            <li class="videos__item">
-             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-            <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-            <h3 class="titulo-video">${video.titulo}</h3>
-            <p class="titulo-canal">${video.descricao}</p>
-            
-            </div>
-            </li>`;
-        })        
+async function buscarVidosAPI() {
+  try {
+    const buscaVideo = await fetch("http://localhost:3000/videos");
+    const videos = await buscaVideo.json();
 
-
-    
+    videos.forEach((video) => {
+      //para cada video vai ser exibido // o innerHTML vai colocar os videos dentro do HTML - cada video vai ser acrescendo
+      containerVideos.innerHTML += `
+                <li class="videos__item">
+                            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                            <div class="descricao-video">
+                                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                                <h3 class="titulo-video">${video.titulo}</h3>
+                                <p class="titulo-canal">${video.descricao}</p>
+                        
+                            </div>
+                        </li>`;
+    });
+  } catch (error) {
+    containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${error}</p>`;
+  }
 }
 buscarVidosAPI();
