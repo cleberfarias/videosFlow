866c8d1cf26b9d9ce76ebc976fcffe64f34501e0
cleberfarias <cleber_afd@hotmail.com>
2025-05-20 11:01:15 -0300

    implementação de logica de pesquisa

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function buscarVidosAPI() {
                                 <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                                 <h3 class="titulo-video">${video.titulo}</h3>
                                 <p class="titulo-canal">${video.descricao}</p>
-                        
+                                <p class="categoria" hidden>${video.categoria}</p>
                             </div>
                         </li>`;
     });
@@ -23,3 +23,50 @@ async function buscarVidosAPI() {
   }
 }
 buscarVidosAPI();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+  const videos = document.querySelectorAll(".videos__item");
+
+  if (barraDePesquisa.value != "") {
+    for (let video of videos) {
+      let title = video
+        .querySelector(".titulo-video")
+        .textContent.toLocaleLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+      if (!title.includes(valorFiltro)) {
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
+    }
+  } else {
+    for (let video of videos) {
+      video.style.display = "block";
+    }
+  }
+}
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+  let nomeCategoria = botao.getAttribute("name");
+  botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+  const videos = document.querySelectorAll(".videos__item");
+  for (let video of videos) {
+    let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+    let valorFiltro = filtro.toLowerCase();
+
+    if (valorFiltro !== "tudo" && !categoria.includes(valorFiltro)) {
+      video.style.display = "none";
+    } else {
+      video.style.display = "block";
+    }
+  }
+}
